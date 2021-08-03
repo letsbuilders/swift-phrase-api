5941b8b68b64b90a3e0a80106515edcc096b1693
Kacper Kawecki <kacper@letsbuild.com>
2021-08-03 15:27:16 +0200

    Fix Tag response

--- Sources/PhraseApi/Api/tag/TagResponse.swift
+++ Sources/PhraseApi/Api/tag/TagResponse.swift
@@ -6,20 +6,20 @@ import Foundation
 
 public struct TagResponse: Decodable {
     public struct Statistic: Decodable {
-        let keys_total_count: Int
-        let translations_completed_count: Int
-        let translations_unverified_count: Int
-        let keys_untranslated_count: Int
+        let keysTotalCount: Int
+        let translationsCompletedCount: Int
+        let translationsUnverifiedCount: Int
+        let keysUntranslatedCount: Int
     }
 
     public struct LocaleStatistic: Decodable {
         let locale: Locale
-        let statistics: Statistic
+        let statistics: Statistic?
     }
 
     let name: String
-    let keyCount: Int
-    let createdAt: Date
+    let keyCount: Int?
+    let createdAt: Date?
     let updatedAt: Date?
-    let statistics: [LocaleStatistic]
+    let statistics: [LocaleStatistic]?
 }
